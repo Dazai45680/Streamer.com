1aa8bc9daa5ca04e822efd536e69d1aa09e0d23b
Dazai45680 <31892373+Dazai45680@users.noreply.github.com>
2019-04-14 00:25:37 +0200

    Add files via upload

--- server/Handling_request.js
+++ server/Handling_request.js
@@ -6,8 +6,8 @@ var Html;
 // item text is the requested image text for the first site loading
 var item_text = [];
 var item_link = [];
-Request('https://www11.gogoanimes.tv/' , (error , response , html) =>{
-      
+Request('https://www14.gogoanimes.tv/', (error, response, html) => {
+
     Html = html;
 });
 
@@ -21,6 +21,13 @@ module.exports = class Handling_Request {
             res.writeHead(200, { 'Content-Type': 'text/html' });
             var RS = fs.createReadStream(__dirname + '/../build/index.html')
             RS.pipe(res);
+        }
+        if (req.url.substring(0, 5) == "/sear") {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            var RS = fs.createReadStream(__dirname + '/../build/index.html')
+            RS.pipe(res);
+            console.log(req.url.substring(0, 5));
+            console.log(req.url.substring(5, req.url.length));
         }
         if (req.url.split('.').pop() == 'css') {
             res.writeHead(200, { 'Content-Type': 'text/css' });
@@ -45,35 +52,84 @@ module.exports = class Handling_Request {
             RS.pipe(res);
         }
         // handling Scrabing request----------------------------------------->
-        if(req.url == '/requesting_items'){
+        if (req.url == '/requesting_items') {
             let $ = cheerio.load(Html);
-            
-            $('.name').each((i ,element)=>{
+
+            $('.name').each((i, element) => {
                 item_text[i] = $(element).children().attr('href').substring(1);
-                
-            });
-            
-            $('.img').each((i ,element)=>{
-                item_link[i] = $(element).find('a img').attr('src');
-                console.log(item_link[i]);
-                
+
             });
 
-            
+            $('.img').each((i, element) => {
+                item_link[i] = $(element).find('a img').attr('src');
+                console.log(item_link[i]);
+
+            });
+
+
             res.end(JSON.stringify(item_text))
-            
+
         }
 
         for (let i = 0; i < item_text.length; i++) {
             const element = '/' + item_text[i];
-            if(req.url == element){
+            if (req.url == element) {
                 //console.log('https://images.gogoanime.tv/cover'+element+'.png');
-                
-               Request(item_link[i]).pipe(res)
+
+                Request(item_link[i]).pipe(res)
+            }
+        }
+        for (let i = 0; i < item_text.length; i++) {
+            const element = '/videos/' + item_text[i];
+
+            if (req.url == element) {
+                console.log('enter');
+                Request('https://www14.gogoanimes.tv/' + item_text[i], (error, response, html) => {
+                    console.log('enter');
+                    let $ = cheerio.load(html);
+                    let video = $('.rapidvideo').find('a').attr('data-video');
+                    console.log(video);
+
+                    Request(video + '&q=720p', (error, response, html) => {
+                        console.log('enter2');
+                        let $ = cheerio.load(html);
+                        let video_mp4 = $('.video-js').find('source').attr('src');
+                        console.log(video_mp4);
+
+                        res.end(JSON.stringify(video_mp4));
+
+
+                    });
+                });
             }
         }
 
-      
-       
+        if(req.url.substring(0, 5) == "/Sear"){
+            console.log(req.url.substring(5, req.url.length));
+             item_text = [];
+             item_link = [];
+            Request('https://www2.gogoanime.io/search.html?keyword=' + req.url.substring(5, req.url.length), (error, response, html) => {
+                console.log('search will be made');
+                let $ = cheerio.load(html);
+                $('.name').each((i, element) => {
+                    item_text[i] = $(element).children().attr('title').replace(/ /g, "-");
+                    console.log(item_text[i]);
+                });
+    
+                $('.img').each((i, element) => {
+                    item_link[i] = $(element).find('a img').attr('src');
+                    //console.log(item_link[i]);
+    
+                });
+    
+                    res.end(JSON.stringify(item_text))
+
+               
+
+            });
+        }
+
+
+
     };
 }
